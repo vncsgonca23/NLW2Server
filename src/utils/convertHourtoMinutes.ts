57ef944829388ca69ac779d15af56ab98dff2e21
Vinicius Gonçalves <vncsgonca23@gmail.com>
2020-08-05 18:55:40 -0300

    feat: Finished Feature

---
+++ src/utils/convertHourtoMinutes.ts
@@ -0,0 +1,7 @@
+function convertHourToMinutes(time:string){
+  const[hour,minutes]=time.split(':').map(Number)
+  const timeInMinutes = (hour*60)+minutes;
+  return timeInMinutes
+}
+
+export {convertHourToMinutes}
